fb1ace35a1ed795b8b83d69be91a46c9fd2b2a38
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-10-18 00:37:25 +0800

    檢查輸入是否為正整數

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,38 @@ namespace 飲料訂單視窗頁面
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<string,int> drinks = new Dictionary<string,int>();
+        Dictionary<string, int> orders = new Dictionary<string, int>();
         public MainWindow()
         {
             InitializeComponent();
+            //增新飲料品項
+            AddNewDrink(drinks);
+
+        }
+
+        //增新飲料品項
+        private void AddNewDrink(Dictionary<string, int> fdrinks)
+        {
+            fdrinks.Add("紅茶大杯", 60);
+            fdrinks.Add("紅茶小杯", 40);
+            fdrinks.Add("綠茶大杯", 60);
+            fdrinks.Add("綠茶小杯", 40);
+            fdrinks.Add("咖啡大杯", 60);
+            fdrinks.Add("咖啡小杯", 40);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var targetTextBox = sender as TextBox;
+
+            bool success = int.TryParse(targetTextBox.Text, out int quantity);
+            if (!success) MessageBox.Show("請輸入整數值", "輸入錯誤");
+            else if (quantity <= 0) MessageBox.Show("請輸入正數數值", "輸入錯誤");
+            else{ 
+                var targetStackPanel = targetTextBox.Parent as StackPanel;
+                var targetLabel = targetStackPanel.Children[0] as Label;
+            }
         }
     }
 }
